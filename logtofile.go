20eda8c0fb9b955a19e14be70c946ccfaf602102
Mydao <hugothiebaut22@gmail.com>
2024-05-18 00:26:53 +0200

    All log methods added

--- logtofile.go
+++ logtofile.go
@@ -1,3 +1,8 @@
+// This file contains the implementation of a logging package that writes log messages to a file.
+// The package provides functions to log messages with different severity levels such as INFO, ERROR, DEBUG, and FATAL.
+// The log messages are written to a file with a timestamp and the severity level.
+// Author: Mydao57
+
 package logtofile
 
 import (
@@ -41,4 +46,40 @@ func (l *Logger) log(severity, message string) {
 	now := time.Now().Format("2006-01-02 15:04:05")
 	logMessage := fmt.Sprintf("[%s] [%s] %s\n", now, severity, message)
 	l.file.WriteString(logMessage)
+}
+
+// INFO logs an information message to the log file.
+func INFO(message string) {
+	if logInstance != nil {
+		logInstance.log("INFO", message)
+	}
+}
+
+// ERROR logs an error message to the log file.
+func ERROR(message string) {
+	if logInstance != nil {
+		logInstance.log("ERROR", message)
+	}
+}
+
+// DEBUG logs a debug message to the log file.
+func DEBUG(message string) {
+	if logInstance != nil {
+		logInstance.log("DEBUG", message)
+	}
+}
+
+// FATAL logs a fatal error message to the log file.
+func FATAL(message string) {
+	if logInstance != nil {
+		logInstance.log("FATAL", message)
+	}
+}
+
+// Close the log file
+func Close() error {
+	if logInstance != nil {
+		return logInstance.file.Close()
+	}
+	return nil
 }
